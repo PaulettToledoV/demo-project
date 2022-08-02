6ffd06086849da2c8855d1266f911364306dade4
Paulett Toledo <paulett.toledo@solera.com>
2022-08-02 16:25:47 -0500

    Add initial comit

---
+++ src/main/java/com/solera/demo_proyect/App.java
@@ -0,0 +1,13 @@
+package com.solera.demo_proyect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
